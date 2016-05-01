19d0154d7e6a2a2da0149f7f14d9f7e3667ff0c4
saikiran638 <saikiran.rguiiit@gmail.com>
2016-05-01 17:16:10 +0530

    update

--- FinalYearProject/makecorpus.sh
+++ FinalYearProject/makecorpus.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
-DIR="/home/saikiran/Desktop/FYproject/bbc/business"
-DIR2="/home/saikiran/Desktop/FYproject"
+DIR="/home/saikiran/Documents/Projects/FinalYearProject/bbc/business"
+DIR2="/home/saikiran/Documents/Projects/FinalYearProject/"
 
-i=10
+i=100
 
-while [ $i -le 50 ]
+while [ $i -le 510 ]
 	do
 	# echo $i
-	cat "$DIR/0$i.txt" >> "$DIR2/testcorpus.txt"
+	cat "$DIR/$i.txt" >> "$DIR2/testdatabusiness.txt"
+	echo "$DIR/00$i.txt"
 	(( i++ ))
 	done
